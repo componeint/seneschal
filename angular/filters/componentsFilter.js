7b229d14ef9959334f604550ea6583c653fbb143
anonymoussc <50c5ac69@opayq.com>
2015-11-24 18:30:34 +0700

    Update componentsFilter

--- angular/filters/componentsFilter.js
+++ angular/filters/componentsFilter.js
@@ -5,13 +5,13 @@
      * @ngdoc service
      * @name app
      * @description
-     * _Please update the description and dependencies._
+     * _Update the description and dependencies._
      *
      * */
-    angular.module('components.filters').filter('componentsFilter', function() {
+    angular.module('components.filters').filter('componentsFilter', [function() {
         return function(input) {
             return input;
         };
-    });
+    }]);
 
 })();

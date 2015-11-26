3092c60cd41c01ca6cffaa6c9e3b52e20173eec8
anonymoussc <50c5ac69@opayq.com>
2015-11-26 20:24:08 +0700

    Update homepage layout

--- angular/applications/jwt_auth/home/home.js
+++ angular/applications/jwt_auth/home/home.js
@@ -1,11 +1,11 @@
 /**
- * Created by anonymoussc on 26/11/15 18:09.
+ * Created by anonymoussc on 26/11/15 19:55.
  */
 
 (function() {
     'use strict';
 
-    angular.module('app.controllers').controller('StaticpageHomeCtrl', ['$scope', function($scope) {
+    angular.module('app.controllers').controller('JwtAuthHomeCtrl', ['$scope', function($scope) {
 
     }]);
 })();

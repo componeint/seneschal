c9d67ec2bb12e5e76cccd316e3c9c8b7001da310
anonymoussc <50c5ac69@opayq.com>
2015-11-26 20:28:50 +0700

    Update right section layout

--- angular/applications/jwt_auth/right/right.js
+++ angular/applications/jwt_auth/right/right.js
@@ -1,11 +1,11 @@
 /**
- * Created by anonymoussc on 26/11/15 18:15.
+ * Created by anonymoussc on 26/11/15 20:27.
  */
 
 (function() {
     'use strict';
 
-    angular.module('app.controllers').controller('StaticpageRightCtrl', ['$scope', '$timeout', '$mdSidenav', '$log', function($scope, $timeout, $mdSidenav, $log) {
+    angular.module('app.controllers').controller('JwtAuthRightCtrl', ['$scope', '$timeout', '$mdSidenav', '$log', function($scope, $timeout, $mdSidenav, $log) {
         $scope.close = function() {
             $mdSidenav('right').close().then(function() {
                 // $log.debug("close RIGHT is done");

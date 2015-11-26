4c11353e4aee7361159a1dd3f8c1049ad5b4c94c
anonymoussc <50c5ac69@opayq.com>
2015-11-26 21:52:46 +0700

    Update simple layout header

--- angular/applications/jwt_auth/header/header.js
+++ angular/applications/jwt_auth/header/header.js
@@ -5,7 +5,12 @@
 (function() {
     'use strict';
 
-    angular.module('app.controllers').controller('JwtAuthHeaderCtrl', ['$scope', function($scope) {
+    angular.module('app.controllers').controller('JwtAuthHeaderCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+        $scope.$watch(function() {
+            return $rootScope.current_page;
+        }, function(newPage) {
+            $scope.current_page = newPage || 'Page Name';
+        });
 
     }]);
 })();

8ece7eb97224dd14ff66bb8a590fbb7090541fac
anonymoussc <50c5ac69@opayq.com>
2015-11-26 20:27:14 +0700

    Update left section layout

--- angular/applications/jwt_auth/left/left.js
+++ angular/applications/jwt_auth/left/left.js
@@ -1,11 +1,11 @@
 /**
- * Created by anonymoussc on 26/11/15 18:13.
+ * Created by anonymoussc on 26/11/15 20:24.
  */
 
 (function() {
     'use strict';
 
-    angular.module('app.controllers').controller('StaticpageLeftCtrl', ['$scope', '$timeout', '$mdSidenav', '$log', function($scope, $timeout, $mdSidenav, $log) {
+    angular.module('app.controllers').controller('JwtAuthLeftCtrl', ['$scope', '$timeout', '$mdSidenav', '$log', function($scope, $timeout, $mdSidenav, $log) {
         $scope.close = function() {
             $mdSidenav('left').close().then(function() {
                 // $log.debug("close LEFT is done");

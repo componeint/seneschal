6d500fa7bc97dfa14cad20657f7e8fa9932135ee
anonymoussc <50c5ac69@opayq.com>
2015-12-01 23:55:52 +0700

    Update routes both business process layer and service component can have its own routes. :+1:

--- angular/routes.js
+++ angular/routes.js
@@ -1,3 +1,96 @@
 /**
  * Created by anonymoussc on 01/12/15 23:48.
  */
+
+/**
+ * Created by anonymoussc on 18/11/15 17:31.
+ */
+
+(function() {
+    'use strict';
+
+    angular.module('components.routes.jwtAuth').config(function($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('jwtauth', {
+                abstract: true,
+                url     : '/auth',
+                views   : {
+                    'layout@'       : {
+                        templateUrl: layout('jwt_auth.simple')
+                    },
+                    'header@jwtauth': {
+                        templateUrl: view('jwt_auth.header')
+                    },
+                    'left@jwtauth'  : {
+                        templateUrl: view('jwt_auth.left')
+                    },
+                    'right@jwtauth' : {
+                        templateUrl: view('jwt_auth.right')
+                    },
+                    'footer@jwtauth': {
+                        templateUrl: view('jwt_auth.footer')
+                    },
+                    'main@jwtauth'  : {}
+                }
+            })
+            .state('jwtauth.signin', {
+                url  : '/signin',
+                data : { pageName: 'Sign-in' },
+                views: {
+                    'main@jwtauth': {
+                        templateUrl: view('jwt_auth.home')
+                    }
+                }
+            });
+
+        function view(viewName) {
+            if (viewName !== "") {
+                return './views/applications/' + appName(viewName) + '/' + fileDir(viewName) + '/' + fileName(viewName) + '.html';
+            } else {
+                return './views/applications/app/home/home.html';
+            }
+        }
+
+        function layout(viewName) {
+            if (viewName !== "") {
+                return './views/themes/' + appName(viewName) + '/' + fileDir(viewName) + '/' + fileName(viewName) + '.html';
+            } else {
+                return './views/applications/app/home/home.html';
+            }
+
+        }
+
+        function appName(v) {
+            if (v.split(".")[0]) {
+                return v.split(".")[0];
+            } else {
+                return 'app';
+            }
+        }
+
+        function fileDir(v) {
+            if (v.split(".")[1]) {
+                return v.split(".")[1];
+            } else if (!v.split(".")[0]) {
+                return v;
+            } else {
+                return 'home';
+            }
+        }
+
+        function fileName(v) {
+            if (v.split(".")[2]) {
+                return v.split(".")[2];
+            } else if (!v.split(".")[2]) {
+                if (v.split(".")[1]) {
+                    return v.split(".")[1];
+                }
+            } else {
+                return 'home';
+            }
+        }
+    });
+})();

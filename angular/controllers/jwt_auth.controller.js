d0a3b549c6aeb0ec28096935b9c796ce67b8df48
anonymoussc <50c5ac69@opayq.com>
2015-12-03 21:30:44 +0700

    Update jwt-auth header layout

--- angular/controllers/jwt_auth.controller.js
+++ angular/controllers/jwt_auth.controller.js
@@ -9,70 +9,13 @@
         .module('components.controllers')
         .controller('JwtAuthController', JwtAuthController);
 
-    JwtAuthController.$inject = ['$scope', '$timeout', '$mdSidenav', '$log'];
+    JwtAuthController.$inject = [];
 
     /* @ngInject */
-    function JwtAuthController($scope, $timeout, $mdSidenav, $log) {
-        var vm         = this;
-        vm.title       = 'JwtAuthController';
-        vm.toggleLeft  = buildDelayedToggler('left');
-        vm.toggleRight = buildToggler('right');
-        vm.isOpenRight = isOpenRight;
+    function JwtAuthController() {
+        var vm   = this;
+        vm.title = 'JwtAuthController';
 
-        /*
-         activate();
-
-         ////////////////
-
-         function activate() {
-         //
-         }
-         */
-
-        /**
-         * Build handler to open/close a SideNav; when animation finishes
-         * report completion in console
-         */
-        function buildDelayedToggler(navID) {
-            return debounce(function() {
-                $mdSidenav(navID)
-                    .toggle()
-                    .then(function() {
-                        // $log.debug("toggle " + navID + " is done");
-                    });
-            }, 200);
-        }
-
-        function buildToggler(navID) {
-            return function() {
-                $mdSidenav(navID)
-                    .toggle()
-                    .then(function() {
-                        // $log.debug("toggle " + navID + " is done");
-                    });
-            };
-        }
-
-        function isOpenRight() {
-            return $mdSidenav('right').isOpen();
-        }
-
-        /**
-         * Supplies a function that will continue to operate until the
-         * time is up.
-         */
-        function debounce(func, wait, context) {
-            var timer;
-            return function debounced() {
-                var context = vm,
-                    args    = Array.prototype.slice.call(arguments);
-                $timeout.cancel(timer);
-                timer       = $timeout(function() {
-                    timer = undefined;
-                    func.apply(context, args);
-                }, wait || 10);
-            };
-        }
     }
 
 })();

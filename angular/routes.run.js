27d455565d16922ae17421979302fa2559e26f92
anonymoussc <50c5ac69@opayq.com>
2015-12-04 09:17:27 +0700

    Update routes.run.js

---
+++ angular/routes.run.js
@@ -0,0 +1,23 @@
+/**
+ * Created by anonymoussc on 04/12/15 9:11.
+ */
+
+(function() {
+    'use strict';
+
+    angular.module('components.routes.jwtAuth').run(function($rootScope) {
+        $rootScope.$on("$stateChangeStart", function(event, toState) {
+            var user = JSON.parse(localStorage.getItem('user'));
+            if (user) {
+                $rootScope.authenticated = true;
+                $rootScope.currentUser   = user;
+
+                if (toState.name === 'jwtauth.signin') {
+                    event.preventDefault();
+                    $state.go('app.home');
+                }
+            }
+        });
+    });
+})();
+

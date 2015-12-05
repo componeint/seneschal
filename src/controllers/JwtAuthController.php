ba7dd88db8ba68a4ec144aaa76aea39d68aacccd
anonymoussc <50c5ac69@opayq.com>
2015-12-05 09:04:20 +0700

    Update JwtAuthController.php

--- src/controllers/JwtAuthController.php
+++ src/controllers/JwtAuthController.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Created by anonymoussc on 03/12/15 4:04.
+ * Created by anonymous on 03/12/15 4:04.
  */
 
 namespace Onderdelen\JwtAuth\Controllers;
@@ -9,6 +9,10 @@ use Illuminate\Http\Request;
 use JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Anwendungen\Application\Controller;
+
+//use App\Http\Requests;
+//use App\Http\Controllers\Controller;
+//use App\User;
 
 class JwtAuthController extends Controller
 {

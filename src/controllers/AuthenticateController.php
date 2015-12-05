655f1c6d9058cabf3d6f8cd8ec2ea80256bb8009
anonymoussc <50c5ac69@opayq.com>
2015-12-05 09:05:21 +0700

    Add AuthenticateController.php

---
+++ src/controllers/AuthenticateController.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Created by anonymous on 05/12/15 9:00.
+ */
+
+namespace Onderdelen\JwtAuth\Controllers;
+
+use Illuminate\Http\Request;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Anwendungen\Application\Controller;
+
+//use App\Http\Requests;
+//use App\Http\Controllers\Controller;
+//use App\User;
+
+/**
+ * Class AuthenticateController
+ * @package App\Http\Controllers
+ */
+class AuthenticateController extends Controller
+{
+    /**
+     * construct
+     */
+    public function __construct()
+    {
+        $this->middleware('jwt.auth', ['except' => ['authenticate']]);
+    }
+
+
+    /**
+     * Return the user
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $users = User::all();
+        //return $users;
+
+        return response()->json($users);
+    }
+
+
+    /**
+     * Return a JWT
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        try {
+            // verify the credentials and create a token for the user
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            // something went wrong
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+        // if no errors are encountered we can return a JWT
+        return response()->json(compact('token'));
+    }
+
+    /**
+     * Return the authenticated user
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getAuthenticatedUser()
+    {
+        try {
+            if (!$user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['user_not_found'], 404);
+            }
+        } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+            return response()->json(['token_expired'], $e->getStatusCode());
+        } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+            return response()->json(['token_invalid'], $e->getStatusCode());
+        } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+            return response()->json(['token_absent'], $e->getStatusCode());
+        }
+        // the token is valid and we have found the user via the sub claim
+        return response()->json(compact('user'));
+    }
+}

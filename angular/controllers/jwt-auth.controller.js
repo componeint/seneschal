78431184d9ab768250a6d0fbc4c8970b2f023e4c
anonymoussc <50c5ac69@opayq.com>
2015-12-06 01:28:51 +0700

    Update jwt-auth.controller.js

--- angular/controllers/jwt-auth.controller.js
+++ angular/controllers/jwt-auth.controller.js
@@ -16,6 +16,30 @@
         var vm   = this;
         vm.title = 'JwtAuthController';
 
+        vm.users;
+        vm.error;
+
+        vm.getUsers = getUsers;
+        vm.signout  = signout;
+
+        function getUsers() {
+            $http.get('api/authenticate').success(function(users) {
+                vm.users = users;
+            }).error(function(error) {
+                vm.error = error;
+            });
+        }
+
+        function signout() {
+            $auth.logout().then(function() {
+                localStorage.removeItem('user');
+
+                $rootScope.authenticated = false;
+                $rootScope.currentUser   = null;
+
+                $state.go('app.home');
+            });
+        }
     }
 
 })();

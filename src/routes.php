419e34947c44314c2fd40b0c61490e668db2ec89
anonymoussc <50c5ac69@opayq.com>
2015-12-05 21:48:09 +0700

    Updates routes.php

--- src/routes.php
+++ src/routes.php
@@ -2,3 +2,15 @@
 /**
  * Created by anonymous on 05/12/15 21:45.
  */
+
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', function ($api) {
+    $api->post('authenticate', 'App\Http\Controllers\AuthenticateController@authenticate');
+});
+
+// Protected with JWT
+$api->version('v1', ['middleware' => 'api.auth'], function ($api) {
+    $api->get('authenticate', 'App\Http\Controllers\AuthenticateController@index');
+    $api->get('authenticate/user', 'App\Http\Controllers\AuthenticateController@getAuthenticatedUser');
+});

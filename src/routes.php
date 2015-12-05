1e25555929deb72a048ce268a8c658df4e6d5417
anonymoussc <50c5ac69@opayq.com>
2015-12-05 21:54:44 +0700

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -6,11 +6,11 @@
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', function ($api) {
-    $api->post('authenticate', 'App\Http\Controllers\AuthenticateController@authenticate');
+    $api->post('authenticate', 'Onderdelen\JwtAuth\Controllers\AuthenticateController@authenticate');
 });
 
 // Protected with JWT
 $api->version('v1', ['middleware' => 'api.auth'], function ($api) {
-    $api->get('authenticate', 'App\Http\Controllers\AuthenticateController@index');
-    $api->get('authenticate/user', 'App\Http\Controllers\AuthenticateController@getAuthenticatedUser');
+    $api->get('authenticate', 'Onderdelen\JwtAuth\Controllers\AuthenticateController@index');
+    $api->get('authenticate/user', 'Onderdelen\JwtAuth\Controllers\AuthenticateController@getAuthenticatedUser');
 });

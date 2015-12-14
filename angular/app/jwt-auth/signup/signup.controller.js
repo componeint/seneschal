43346f1828040dacc9d3ae7a5f07f8117760c4d1
anonymoussc <50c5ac69@opayq.com>
2015-12-14 15:50:38 +0700

    Add signup

---
+++ angular/app/jwt-auth/signup/signup.controller.js
@@ -0,0 +1,57 @@
+/**
+ * Created by anonymous on 10/12/15 21:24.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('jwtAuth')
+        .controller('JwtAuthSignupController', JwtAuthSignupController);
+
+    JwtAuthSignupController.$inject = ['$location', '$auth', 'toastr'];
+
+    /* @ngInject */
+    function JwtAuthSignupController($location, $auth, toastr) {
+        var vm    = this;
+        vm.title  = 'JwtAuthSignupController';
+        vm.user;
+        vm.signup = signup;
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            //
+        }
+
+        function signup() {
+            $auth.signup(vm.user)
+                .then(function(response) {
+                    $auth.setToken(response);
+                    $location.path('/');
+                    toastr.info('You have successfully created a new account and have been signed-in');
+                })
+                .catch(function(response) {
+                    toastr.error(response.data.message);
+                });
+        }
+    }
+
+})();
+
+/*angular.module('jwtAuth')
+ .controller('SignupCtrl', function($scope, $location, $auth, toastr) {
+ $scope.signup = function() {
+ $auth.signup($scope.user)
+ .then(function(response) {
+ $auth.setToken(response);
+ $location.path('/');
+ toastr.info('You have successfully created a new account and have been signed-in');
+ })
+ .catch(function(response) {
+ toastr.error(response.data.message);
+ });
+ };
+ });*/

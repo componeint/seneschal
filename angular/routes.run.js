9b43f7a99025fdc988ddd2974e29d0366be95348
anonymoussc <50c5ac69@opayq.com>
2015-12-14 09:19:08 +0700

    Update routes.run.js

--- angular/routes.run.js
+++ angular/routes.run.js
@@ -6,7 +6,7 @@
     'use strict';
 
     angular.module('jwtAuth').run(function($rootScope, $state) {
-        $rootScope.$on("$stateChangeStart", function(event, toState) {
+        $rootScope.$on('$stateChangeStart', function(event, toState) {
             var user = JSON.parse(localStorage.getItem('user'));
             if (user) {
                 $rootScope.authenticated = true;

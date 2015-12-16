791e9a5078c1e1d562be208b3c34f7645cc5973f
anonymoussc <50c5ac69@opayq.com>
2015-12-16 14:19:51 +0700

    Add create.controller.js

---
+++ angular/dashboard/users/create/create.controller.js
@@ -0,0 +1,29 @@
+/**
+ * Created by anonymous on 16/12/15 14:12.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('jwtAuth')
+        .controller('UserCreateController', UserCreateController);
+
+    UserCreateController.$inject = [''];
+
+    /* @ngInject */
+    function UserCreateController() {
+        var vm   = this;
+        vm.title = 'UserCreateController';
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            //
+        }
+    }
+
+})();
+

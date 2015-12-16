239876c3e56c85a86685eb0b220aa8eb92876da6
anonymoussc <50c5ac69@opayq.com>
2015-12-16 14:38:00 +0700

    Add index.controller.js @ user

---
+++ angular/dashboard/users/index/index.controller.js
@@ -0,0 +1,29 @@
+/**
+ * Created by anonymous on 16/12/15 14:35.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('jwtAuth')
+        .controller('UserIndexController', UserIndexController);
+
+    UserIndexController.$inject = [''];
+
+    /* @ngInject */
+    function UserIndexController() {
+        var vm   = this;
+        vm.title = 'UserIndexController';
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            //
+        }
+    }
+
+})();
+

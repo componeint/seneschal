bf38ec4d7fd50d11be9db27fbf02b9bc2d5d9107
anonymoussc <50c5ac69@opayq.com>
2015-12-24 19:17:15 +0700

    Update crud-table @ play icon # 1

--- angular/layouts/templates/crud-table/crud-table.js
+++ angular/layouts/templates/crud-table/crud-table.js
@@ -1,5 +1,5 @@
 'use strict';
-	
+
 /**
 * @fileOverview
 * @author Guido Donnari
@@ -12,45 +12,45 @@
 var crudTable = angular.module('jwtAuth');
 
 crudTable.controller('crudTableCtrl', ['$scope', 'gettext', function($scope, gettext) {
-	
+
 	$scope.ct.busy = false;
 	$scope.ct.page = 1;
-	
+
 	$scope.$on('flStartOp', function($e, $args)
 	{
 		$scope.ct.busy = true;
 	});
-	
+
 	$scope.$on('flComplete', function($e, $args)
 	{
 		$scope.ct.busy = false;
 	});
-	
+
 	$scope.ct.pageChanged = function() {
 		$scope.list.changePage($scope.list.pagination_info.currpage);
 	};
-	
+
 	$scope.ct.setSearch = function (field) {
 		$scope.ct.search_field = field;
 	};
-	
+
 	$scope.ct.colIsSortable = function (col) {
 		if (col.sortable == false)	return false;
 		return $scope.list.sortEnabled();
 	};
-	
+
 	$scope.ct.search = function () {
-		
+
 		var v_where = [];
-		
+
 		if ($scope.ct.search_field && $scope.ct.search_value)
 			v_where = [{
-				field: $scope.ct.search_field.field, 
-				condition: $scope.ct.search_field.condition, 
+				field: $scope.ct.search_field.field,
+				condition: $scope.ct.search_field.condition,
 				value: $scope.ct.search_value,
 				options: $scope.ct.search_field.options
 			}];
-		
+
 		if ($scope.list.options.jsonFile)
 			$scope.list.change({
 				where: v_where
@@ -60,30 +60,53 @@ crudTable.controller('crudTableCtrl', ['$scope', 'gettext', function($scope, get
 				where: v_where
 			}, true);
 	};
-	
+
 	$scope.ct.sort = function (e, sort_field, sort_type) {
-		
+
 		if ($scope.list.options.jsonFile)
 			$scope.list.change({
 				orderby: [{
-					field: sort_field, 
+					field: sort_field,
 					type: sort_type
 				}]
 			});
 		else
 			$scope.list.loadData({
 				orderby: [{
-					field: sort_field, 
+					field: sort_field,
 					type: sort_type
 				}]
 			});
 	};
-	
+
 	$scope.ct.getCheckedStr = function() {
-		if ($scope.list.getToggleSelNextState())	return gettext('Uncheck all rows');
-		return gettext('Check all rows');
+		if ($scope.list.getToggleSelNextState()) {
+            return gettext('Uncheck all rows');
+        }
+        else {
+            return gettext('Check all rows');
+        }
 	};
-	
+
+    $scope.ct.getCheckedIcon = function(ico, morphTo) {
+        if ($scope.list.getToggleSelNextState()) {
+            return morphTo;
+        }
+        else {
+            return ico;
+        }
+        //return ico;
+    };
+    $scope.ct.iconMorphTo = function(ico, morphTo) {
+        //$scope.ct.icon = morphTo;
+        if ($scope.ct.icon === ico) {
+            $scope.ct.icon = morphTo;
+        }
+        else {
+            $scope.ct.icon = ico;
+        }
+    };
+
 }])
 .directive('crudTable', function() {
 	return {

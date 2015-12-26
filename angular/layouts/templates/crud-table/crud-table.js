aa4278d52dc8139089e1b819a3327c384931341f
anonymoussc <50c5ac69@opayq.com>
2015-12-26 23:47:56 +0700

    Update crud-table

--- angular/layouts/templates/crud-table/crud-table.js
+++ angular/layouts/templates/crud-table/crud-table.js
@@ -1,134 +1,136 @@
 'use strict';
 
 /**
-* @fileOverview
-* @author Guido Donnari
-*/
+ * @fileOverview
+ * @author Guido Donnari
+ */
 
 /**
-* CRUD template for FlexyList angular module.
-* @module
-*/
+ * CRUD template for FlexyList angular module.
+ * @module
+ */
 var crudTable = angular.module('jwtAuth');
 
 crudTable.controller('crudTableCtrl', ['$scope', 'gettext', function($scope, gettext) {
 
-	$scope.ct.busy = false;
-	$scope.ct.page = 1;
+    $scope.ct.busy = false;
+    $scope.ct.page = 1;
 
-	$scope.$on('flStartOp', function($e, $args)
-	{
-		$scope.ct.busy = true;
-	});
+    $scope.$on('flStartOp', function($e, $args) {
+        $scope.ct.busy = true;
+    });
 
-	$scope.$on('flComplete', function($e, $args)
-	{
-		$scope.ct.busy = false;
-	});
+    $scope.$on('flComplete', function($e, $args) {
+        $scope.ct.busy = false;
+    });
 
-	$scope.ct.pageChanged = function() {
-		$scope.list.changePage($scope.list.pagination_info.currpage);
-	};
+    $scope.ct.pageChanged = function() {
+        $scope.list.changePage($scope.list.pagination_info.currpage);
+    };
 
-	$scope.ct.setSearch = function (field) {
-		$scope.ct.search_field = field;
-	};
+    $scope.ct.setSearch = function(field) {
+        $scope.ct.search_field = field;
+    };
 
-	$scope.ct.colIsSortable = function (col) {
-		if (col.sortable == false)	return false;
-		return $scope.list.sortEnabled();
-	};
-
-	$scope.ct.search = function () {
-
-		var v_where = [];
-
-		if ($scope.ct.search_field && $scope.ct.search_value)
-			v_where = [{
-				field: $scope.ct.search_field.field,
-				condition: $scope.ct.search_field.condition,
-				value: $scope.ct.search_value,
-				options: $scope.ct.search_field.options
-			}];
-
-		if ($scope.list.options.jsonFile)
-			$scope.list.change({
-				where: v_where
-			});
-		else
-			$scope.list.loadData({
-				where: v_where
-			}, true);
-	};
-
-	$scope.ct.sort = function (e, sort_field, sort_type) {
-
-		if ($scope.list.options.jsonFile)
-			$scope.list.change({
-				orderby: [{
-					field: sort_field,
-					type: sort_type
-				}]
-			});
-		else
-			$scope.list.loadData({
-				orderby: [{
-					field: sort_field,
-					type: sort_type
-				}]
-			});
-	};
-
-	$scope.ct.getCheckedStr = function() {
-		if ($scope.list.getToggleSelNextState()) {
-            return gettext('Uncheck all rows');
+    $scope.ct.colIsSortable = function(col) {
+        if (col.sortable === false) {
+            return false;
         }
-        else {
+
+        return $scope.list.sortEnabled();
+    };
+
+    $scope.ct.search = function() {
+
+        var v_where = [];
+
+        if ($scope.ct.search_field && $scope.ct.search_value) {
+            v_where = [{
+                field    : $scope.ct.search_field.field,
+                condition: $scope.ct.search_field.condition,
+                value    : $scope.ct.search_value,
+                options  : $scope.ct.search_field.options
+            }];
+        }
+
+        if ($scope.list.options.jsonFile) {
+            $scope.list.change({
+                where: v_where
+            });
+        } else {
+            $scope.list.loadData({
+                where: v_where
+            }, true);
+        }
+    };
+
+    $scope.ct.sort = function(e, sort_field, sort_type) {
+        if ($scope.list.options.jsonFile) {
+            $scope.list.change({
+                orderby: [{
+                    field: sort_field,
+                    type : sort_type
+                }]
+            });
+        } else {
+            $scope.list.loadData({
+                orderby: [{
+                    field: sort_field,
+                    type : sort_type
+                }]
+            });
+        }
+    };
+
+    $scope.ct.getCheckedStr = function() {
+        if ($scope.list.getToggleSelNextState()) {
+            return gettext('Uncheck all rows');
+        } else {
             return gettext('Check all rows');
         }
-	};
+    };
 
     $scope.ct.getCheckedIcon = function(ico, morphTo) {
         if ($scope.list.getToggleSelNextState()) {
             return morphTo;
-        }
-        else {
+        } else {
             return ico;
         }
         //return ico;
     };
-    $scope.ct.iconMorphTo = function(ico, morphTo) {
+
+    $scope.ct.iconMorphTo    = function(ico, morphTo) {
         //$scope.ct.icon = morphTo;
         if ($scope.ct.icon === ico) {
             $scope.ct.icon = morphTo;
-        }
-        else {
+        } else {
             $scope.ct.icon = ico;
         }
     };
 
 }])
-.directive('crudTable', function() {
-	return {
-		restrict: 'E',
-		scope: {
-			list: '=',
-			ct: '='
-		},
-		controller: 'crudTableCtrl',
-		templateUrl: function(elem, attr){
-			return attr.template;
-		}
-	};
-})
-.directive('flFieldDisplay', function() {
-	return {
-		restrict: 'E',
-		link: function link(scope, element, attrs) {
-			if (scope.ct.options.displayField)
-				scope.ct.options.displayField(scope, element, attrs);
-			else
-				element.html(attrs['value']);
-		}
-	};
-});
+    .directive('crudTable', function() {
+        return {
+            restrict   : 'E',
+            scope      : {
+                list: '=',
+                ct  : '='
+            },
+            controller : 'crudTableCtrl',
+            templateUrl: function(elem, attr) {
+                return attr.template;
+            }
+        };
+    })
+    .directive('flFieldDisplay', function() {
+        return {
+            restrict: 'E',
+            link    : function link(scope, element, attrs) {
+                if (scope.ct.options.displayField) {
+                    scope.ct.options.displayField(scope, element, attrs);
+                } else {
+                    element.html(attrs['value']);
+                }
+            }
+        };
+    });

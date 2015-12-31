f676f8a6790722d6f1f85c57d31f4745dd6383bb
anonymoussc <50c5ac69@opayq.com>
2016-01-01 02:47:15 +0700

    New ResetPasswordRequest

---
+++ src/JwtAuth/FormRequests/ResetPasswordRequest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by anonymous on 01/01/16 2:45.
+ */
+
+namespace Onderdelen\JwtAuth\FormRequests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ResetPasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'password' => 'required|min:8|confirmed',
+        ];
+    }
+}

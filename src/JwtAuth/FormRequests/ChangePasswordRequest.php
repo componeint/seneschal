1fa759f661a7f72bca133575bf3d600c8bb931b8
anonymoussc <50c5ac69@opayq.com>
2016-01-01 01:28:20 +0700

    New ChangePasswordRequest

---
+++ src/JwtAuth/FormRequests/ChangePasswordRequest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by anonymous on 01/01/16 1:25.
+ */
+
+namespace Onderdelen\JwtAuth\FormRequests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ChangePasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'oldPassword'              => 'min:8',
+            'newPassword'              => 'required|min:8|confirmed',
+            'newPassword_confirmation' => 'required',
+        ];
+    }
+}
+

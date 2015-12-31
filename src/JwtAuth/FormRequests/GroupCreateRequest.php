bc62c9d911aca798526c22259556b6780a805c66
anonymoussc <50c5ac69@opayq.com>
2016-01-01 02:35:31 +0700

    New GroupCreateRequest

---
+++ src/JwtAuth/FormRequests/GroupCreateRequest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by anonymous on 01/01/16 2:33.
+ */
+
+namespace Onderdelen\JwtAuth\FormRequests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class GroupCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:4|unique:groups'
+        ];
+    }
+}

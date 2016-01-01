8afe91dcfbc401c97f90852136025bb26f9769f1
anonymoussc <50c5ac69@opayq.com>
2016-01-01 16:34:31 +0700

    New signin-form

---
+++ angular/directives/signin-form/signin-form.directive.js
@@ -0,0 +1,39 @@
+/**
+ * Created by anonymous on 01/01/16 15:01.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('jwtAuth')
+        .directive('signinForm', signinForm);
+
+    signinForm.$inject = [];
+
+    /* @ngInject */
+    function signinForm() {
+        var directive = {
+            bindToController: true,
+            controller      : SigninFormController,
+            controllerAs    : 'signinFrm',
+            link            : link,
+            restrict        : 'A',
+            scope           : {}
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+
+        }
+    }
+
+    SigninFormController.$inject = [];
+
+    /* @ngInject */
+    function SigninFormController() {
+
+    }
+
+})();
+

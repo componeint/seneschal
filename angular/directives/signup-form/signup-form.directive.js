ca11d31fac6fb8847532261dd7aa92c9ccd409ef
anonymoussc <50c5ac69@opayq.com>
2016-01-02 21:35:44 +0700

    New signup-form @ directive

---
+++ angular/directives/signup-form/signup-form.directive.js
@@ -0,0 +1,62 @@
+/**
+ * Created by anonymous on 02/01/16 21:12.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('jwtAuth')
+        .directive('signupForm', signupForm);
+
+    signupForm.$inject = [];
+
+    /* @ngInject */
+    function signupForm() {
+        var directive = {
+            bindToController: true,
+            controller      : SignupFormController,
+            controllerAs    : 'SignupForm',
+            link            : link,
+            restrict        : 'EA',
+            scope           : {},
+            templateUrl     : function(elem, attr) {
+                return attr.template;
+            }
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+
+        }
+    }
+
+    SignupFormController.$inject = ['$location', '$state', '$auth', 'toastService'];
+
+    /* @ngInject */
+    function SignupFormController($location, $state, $auth, toastService) {
+        var vm    = this;
+        vm.title  = 'JwtAuthSignupController';
+        vm.user;
+        vm.signup = signup;
+
+        ////////////////
+
+        function signup() {
+            $auth
+                .signup(vm.user)
+                .then(function(response) {
+                    $auth.setToken(response);
+                    $state.go('jwtauth.home');
+                    toastService.show('You have successfully created a new account and have been signed-in');
+                })
+                .catch(function(response) {
+                    console.log(response.data.message);
+                    toastService.error(response.data.message);
+                });
+        }
+
+    }
+
+})();
+

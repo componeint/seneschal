9465d7654ff33275d27f2f06501c698eab86cbda
anonymoussc <50c5ac69@opayq.com>
2016-01-03 04:47:16 +0700

    New sidenav

---
+++ angular/app/jwt-auth/sidenav/sidenav.controller.js
@@ -0,0 +1,34 @@
+/**
+ * Created by anonymous on 03/01/16 4:41.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('jwtAuth')
+        .controller('JwtAuthSidenavController', JwtAuthSidenavController);
+
+    JwtAuthSidenavController.$inject = ['$timeout', '$mdSidenav', '$log'];
+
+    /* @ngInject */
+    function JwtAuthSidenavController($timeout, $mdSidenav, $log) {
+        var vm   = this;
+        vm.title = 'JwtAuthSidenavController';
+        vm.close = function() {
+            $mdSidenav('left').close().then(function() {
+                // $log.debug("close LEFT is done");
+            });
+        };
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            //
+        }
+    }
+
+})();
+

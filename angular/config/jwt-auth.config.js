3d9886f884dfbc736b0189c1b48dace0b41f2e69
anonymoussc <50c5ac69@opayq.com>
2016-01-04 21:44:17 +0700

    Modified jwt-auth.config

--- angular/config/jwt-auth.config.js
+++ angular/config/jwt-auth.config.js
@@ -1,6 +1,7 @@
 /**
  * Created by anonymous on 06/12/15 5:02.
  */
+
 (function() {
     'use strict';
 
@@ -11,9 +12,11 @@
     jwtAuth.$inject = ['$authProvider'];
 
     /* @ngInject */
-    function jwtAuth ($authProvider) {
-        $authProvider.loginUrl = '/api/authenticate';
-        $authProvider.signupUrl = '/api/auth/signup';
+    function jwtAuth($authProvider) {
+        $authProvider.loginUrl    = '/api/authenticate';
+        $authProvider.signupUrl   = '/api/auth/signup';
+        $authProvider.tokenName   = 'token';
+        $authProvider.tokenPrefix = 'app';
     }
 
 })();

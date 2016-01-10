f991b5fc4d0a6a174103b7dea56d456e581f1d70
anonymoussc <50c5ac69@opayq.com>
2016-01-10 09:29:39 +0700

    Modified users-data-table.directive, users-data-table.html

--- angular/directives/users-data-table/users-data-table.directive.js
+++ angular/directives/users-data-table/users-data-table.directive.js
@@ -28,11 +28,81 @@
         }
     }
 
-    UsersDataTableController.$inject = [''];
+    UsersDataTableController.$inject = ['$http', '$mdEditDialog', '$q', '$timeout'];
 
     /* @ngInject */
-    function UsersDataTableController() {
+    function UsersDataTableController($http, $mdEditDialog, $q, $timeout) {
+        var vm        = this;
+        vm.onPaginate = onPaginate;
+        vm.deselect   = deselect;
+        vm.log        = log;
+        vm.loadStuff  = loadStuff;
+        vm.onReorder  = onReorder;
 
+        activate();
+
+        ////////////////
+
+        function activate() {
+            $http.get('api/groups').then(function(responses) {
+                vm.records = responses.data;
+                // $timeout(function () {
+                //   vm.records = responses.data;
+                // }, 1000);
+            });
+        }
+
+        vm.selected = [];
+
+        vm.query = {
+            order: 'id',
+            limit: 5,
+            page : 1
+        };
+
+        vm.columns = [
+            {
+                name   : 'Name',
+                orderBy: 'name'
+            },
+            {
+                name   : 'Permissions',
+                orderBy: 'permissions'
+            }
+        ];
+
+        function onPaginate(page, limit) {
+            console.log('Scope Page: ' + vm.query.page + ' Scope Limit: ' + vm.query.limit);
+            console.log('Page: ' + page + ' Limit: ' + limit);
+
+            vm.promise = $timeout(function() {
+
+            }, 2000);
+        }
+
+        function deselect(item) {
+            console.log(item.name, 'was deselected');
+        }
+
+        function log(item) {
+            console.log(item.name, 'was selected');
+        }
+
+        function loadStuff() {
+            vm.promise = $timeout(function() {
+
+            }, 2000);
+        }
+
+        function onReorder(order) {
+
+            console.log('Scope Order: ' + vm.query.order);
+            console.log('Order: ' + order);
+
+            vm.promise = $timeout(function() {
+
+            }, 2000);
+        }
     }
 
 })();

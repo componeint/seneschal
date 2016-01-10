6b53c3158cf2d50688721fb359bfa3fafe8dd286
anonymoussc <50c5ac69@opayq.com>
2016-01-11 00:12:29 +0700

    Modified group-create-form.directive, group-create-form

--- angular/directives/group-create-form/group-create-form.directive.js
+++ angular/directives/group-create-form/group-create-form.directive.js
@@ -1,3 +1,55 @@
 /**
  * Created by anonymous on 31/12/15 12:59.
  */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('jwtAuth')
+        .directive('groupCreateForm', groupCreateForm);
+
+    groupCreateForm.$inject = [];
+
+    /* @ngInject */
+    function groupCreateForm() {
+        var directive = {
+            bindToController: true,
+            controller      : GroupCreateFormController,
+            controllerAs    : 'vm',
+            link            : link,
+            restrict        : 'EA',
+            scope           : {},
+            templateUrl     : function(elem, attr) {
+                return attr.template;
+            }
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+
+        }
+    }
+
+    GroupCreateFormController.$inject = [];
+
+    /* @ngInject */
+    function GroupCreateFormController() {
+        var vm                = this;
+        vm.defaultPermissions = [
+            {name: 'admin'},
+            {name: 'users'}
+        ];
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            //
+        }
+
+    }
+
+})();
+

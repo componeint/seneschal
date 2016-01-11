3ef5da5eff44d760ba2800b46e7a1e726ae19c5e
anonymoussc <50c5ac69@opayq.com>
2016-01-11 19:36:45 +0700

    Modified group-create-form.directive, group-create-form

--- angular/directives/group-create-form/group-create-form.directive.js
+++ angular/directives/group-create-form/group-create-form.directive.js
@@ -31,15 +31,30 @@
         }
     }
 
-    GroupCreateFormController.$inject = [];
+    GroupCreateFormController.$inject = ['API', 'ToastService'];
 
     /* @ngInject */
-    function GroupCreateFormController() {
+    function GroupCreateFormController(API, ToastService) {
         var vm                = this;
         vm.defaultPermissions = [
-            {name: 'admin'},
-            {name: 'users'}
+            {name: 'admin', value: 1},
+            {name: 'users', value: 1}
         ];
+        vm.selected           = [];
+        vm.toggle             = function(item, list) {
+            var idx = list.indexOf(item);
+            if (idx > -1) {
+                list.splice(idx, 1);
+            }
+            else {
+                list.push(item);
+                //Object.assign({}, list.push(item));
+            }
+        };
+        vm.exists             = function(item, list) {
+            return list.indexOf(item) > -1;
+        };
+        vm.groupCreate        = groupCreate;
 
         activate();
 
@@ -47,6 +62,21 @@
 
         function activate() {
             //
+        }
+
+        function groupCreate() {
+            vm.formData = {
+                name       : vm.name,
+                permissions: vm.selected
+            };
+
+            /*
+            API.all('posts').post(vm.formData).then(function(response){
+             ToastService.show('Post added successfully');
+             });
+             */
+
+            console.log(vm.formData);
         }
 
     }

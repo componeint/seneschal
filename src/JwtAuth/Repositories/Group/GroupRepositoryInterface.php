cae76b7083c708f21df905ab16f1079c8e42c664
anonymoussc <50c5ac69@opayq.com>
2016-01-13 20:42:17 +0700

    Modified GroupRepositoryInterface

--- src/JwtAuth/Repositories/Group/GroupRepositoryInterface.php
+++ src/JwtAuth/Repositories/Group/GroupRepositoryInterface.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * JwtAuthGroupRepositoryInterface.php
+ * GroupRepositoryInterface.php
  * Created by anonymous on 18/12/15 8:39.
  */
 
@@ -14,7 +14,7 @@ use Onderdelen\JwtAuth\Models\User;
  * Interface GroupRepositoryInterface
  * @package Onderdelen\JwtAuth\Repositories\Group
  */
-interface JwtAuthGroupRepositoryInterface
+interface GroupRepositoryInterface
 {
     /**
      * Store a newly created resource in storage.

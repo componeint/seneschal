01651866b744826c0923e20be3e3c227b5925c08
anonymoussc <50c5ac69@opayq.com>
2016-01-13 21:27:07 +0700

    Modified User

--- src/JwtAuth/Models/User.php
+++ src/JwtAuth/Models/User.php
@@ -1,11 +1,12 @@
 <?php
 /**
+ * User.php
  * Created by anonymous on 05/12/15 9:10.
  */
 
 namespace Onderdelen\JwtAuth\Models;
 
-use \Sentinel\Models\User as BaseUser;
+use \Cerberus\Models\User as BaseUser;
 
 class User extends BaseUser
 {

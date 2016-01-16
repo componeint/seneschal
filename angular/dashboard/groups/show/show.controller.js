dbb92c5719f16bf5903467da11ea33c93f589c8c
anonymoussc <50c5ac69@opayq.com>
2016-01-16 22:29:51 +0700

    Modified show.controller

--- angular/dashboard/groups/show/show.controller.js
+++ angular/dashboard/groups/show/show.controller.js
@@ -1,4 +1,5 @@
 /**
+ * show.controller.js
  * Created by anonymous on 29/12/15 21:07.
  */
 
@@ -9,12 +10,12 @@
         .module('jwtAuth')
         .controller('GroupShowController', GroupShowController);
 
-    GroupShowController.$inject = [];
+    GroupShowController.$inject = ['$stateParams'];
 
     /* @ngInject */
-    function GroupShowController() {
-        var vm   = this;
-        vm.title = 'GroupShowController';
+    function GroupShowController($stateParams) {
+        var vm     = this;
+        vm.groupId = $stateParams.groupId;
 
         activate();
 

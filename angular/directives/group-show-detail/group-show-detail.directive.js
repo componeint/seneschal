9a3675fe74eef515736033c3c94fd23f29505812
anonymoussc <50c5ac69@opayq.com>
2016-01-16 20:33:34 +0700

    New group-show-detail

---
+++ angular/directives/group-show-detail/group-show-detail.directive.js
@@ -0,0 +1,43 @@
+/**
+ * group-show-detail.directive.js
+ * Created by anonymous on 16/01/16 14:54.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('jwtAuth')
+        .directive('groupShowDetail', groupShowDetail);
+
+    groupShowDetail.$inject = [];
+
+    /* @ngInject */
+    function groupShowDetail() {
+        var directive = {
+            bindToController: true,
+            controller      : GroupShowDetailController,
+            controllerAs    : 'vm',
+            link            : link,
+            restrict        : 'EA',
+            scope           : {},
+            templateUrl     : function(elem, attr) {
+                return attr.template;
+            }
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+
+        }
+    }
+
+    GroupShowDetailController.$inject = [];
+
+    /* @ngInject */
+    function GroupShowDetailController() {
+
+    }
+
+})();
+

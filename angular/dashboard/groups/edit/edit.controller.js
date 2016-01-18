8c0115117a201ef9b25422ac4d4037ba3e5d4568
anonymoussc <50c5ac69@opayq.com>
2016-01-18 08:39:51 +0700

    Modified edit.controller

--- angular/dashboard/groups/edit/edit.controller.js
+++ angular/dashboard/groups/edit/edit.controller.js
@@ -1,4 +1,5 @@
 /**
+ * edit.controller.js
  * Created by anonymous on 29/12/15 21:09.
  */
 
@@ -9,19 +10,21 @@
         .module('jwtAuth')
         .controller('GroupEditController', GroupEditController);
 
-    GroupEditController.$inject = [];
+    GroupEditController.$inject = ['$stateParams', 'Groups'];
 
     /* @ngInject */
-    function GroupEditController() {
-        var vm   = this;
-        vm.title = 'GroupEditController';
+    function GroupEditController($stateParams, Groups) {
+        var vm     = this;
+        vm.groupId = $stateParams.id;
 
         activate();
 
         ////////////////
 
         function activate() {
-            //
+            Groups.get(vm.groupId).then(function(response) {
+                vm.groups = response.data;
+            });
         }
     }
 

febbacb975bd93b5cc49c0eb838c323bf0d49199
anonymoussc <50c5ac69@opayq.com>
2016-01-20 09:49:36 +0700

    Modified routes

--- src/routes.php
+++ src/routes.php
@@ -7,77 +7,76 @@
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', function ($api) {
-    $api->post('auth/signin', 'Onderdelen\JwtAuth\Controllers\AuthenticateController@authenticate');
-    $api->post('auth/signup',
-        ['as' => 'register.user', 'uses' => 'Onderdelen\JwtAuth\Controllers\RegistrationController@register']);
-    $api->get('users', ['as' => 'users.index', 'uses' => 'Onderdelen\JwtAuth\Controllers\UserController@index']);
-    $api->get('users/{hash}',
-        ['as' => 'users.show', 'uses' => 'Onderdelen\JwtAuth\Controllers\UserController@show']);
+    /*
+     * Registration
+     *
+     */
+    // // $api->get('register', ['as' => 'register.form', 'uses' => 'Onderdelen\JwtAuth\Controllers\RegistrationController@registration']);
+    // $api->post('register', ['as' => 'register.user', 'uses' => 'Onderdelen\JwtAuth\Controllers\RegistrationController@register']);
+    $api->post('auth/signup', ['as' => 'register.user', 'uses' => 'Onderdelen\JwtAuth\Controllers\RegistrationController@register']);
+    $api->get('users/activate/{hash}/{code}', ['as' => 'activate', 'uses' => 'Onderdelen\JwtAuth\Controllers\RegistrationController@activate']);
+    // // $api->get('reactivate', ['as' => 'reactivate.form', 'uses' => 'Onderdelen\JwtAuth\Controllers\RegistrationController@resendActivationForm']);
+    $api->post('reactivate', ['as' => 'reactivate.send', 'uses' => 'Onderdelen\JwtAuth\Controllers\RegistrationController@resendActivation']);
+    $api->get('forgot', ['as' => 'forgot.form', 'uses' => 'Onderdelen\JwtAuth\Controllers\RegistrationController@forgotPasswordForm']);
+    $api->post('forgot', ['as' => 'reset.request', 'uses' => 'Onderdelen\JwtAuth\Controllers\RegistrationController@sendResetPasswordEmail']);
+    // // $api->get('reset/{hash}/{code}', ['as' => 'reset.form', 'uses' => 'Onderdelen\JwtAuth\Controllers\RegistrationController@passwordResetForm']);
+    $api->post('reset/{hash}/{code}', ['as' => 'reset.password', 'uses' => 'Onderdelen\JwtAuth\Controllers\RegistrationController@resetPassword']);
 
+    /*
+     * Authentication
+     *
+     */
+    $api->post('auth/signin', 'Onderdelen\JwtAuth\Controllers\AuthenticateController@authenticate');
+
+    /*
+     * Profile
+     *
+     */
+    $api->get('profile', ['as' => 'profile.show', 'uses' => 'Onderdelen\JwtAuth\Controllers\ProfileController@show']);
+    $api->get('profile/edit', ['as' => 'profile.edit', 'uses' => 'Onderdelen\JwtAuth\Controllers\ProfileController@edit']);
+    $api->put('profile', ['as' => 'profile.update', 'uses' => 'Onderdelen\JwtAuth\Controllers\ProfileController@update']);
+    $api->post('profile/password', ['as' => 'profile.password', 'uses' => 'Onderdelen\JwtAuth\Controllers\ProfileController@changePassword']);
+
+    /*
+     * Users
+     *
+     */
+
+    $api->get('users', ['as' => 'users.index', 'uses' => 'Onderdelen\JwtAuth\Controllers\UserController@index']);
+    // // $api->get('users/create', ['as' => 'users.create', 'uses' => 'Onderdelen\JwtAuth\Controllers\UserController@create']);
+    $api->post('users', ['as' => 'users.store', 'uses' => 'Onderdelen\JwtAuth\Controllers\UserController@store']);
+
+    $api->get('users/{hash}', ['as' => 'users.show', 'uses' => 'Onderdelen\JwtAuth\Controllers\UserController@show']);
+    $api->get('users/{hash}/edit', ['as' => 'users.edit', 'uses' => 'Onderdelen\JwtAuth\Controllers\UserController@edit']);
+    $api->post('users/{hash}/password', ['as' => 'password.change', 'uses' => 'Onderdelen\JwtAuth\Controllers\UserController@changePassword']);
+    $api->post('users/{hash}/memberships', ['as' => 'users.memberships', 'uses' => 'Onderdelen\JwtAuth\Controllers\UserController@updateGroupMemberships']);
+    $api->put('users/{hash}', ['as' => 'users.update', 'uses' => 'Onderdelen\JwtAuth\Controllers\UserController@update']);
+    $api->delete('users/{hash}', ['as' => 'users.destroy', 'uses' => 'Onderdelen\JwtAuth\Controllers\UserController@destroy']);
+    $api->get('users/{hash}/suspend', ['as' => 'users.suspend', 'uses' => 'Onderdelen\JwtAuth\Controllers\UserController@suspend']);
+    $api->get('users/{hash}/unsuspend', ['as' => 'users.unsuspend', 'uses' => 'Onderdelen\JwtAuth\Controllers\UserController@unsuspend']);
+    $api->get('users/{hash}/ban', ['as' => 'users.ban', 'uses' => 'Onderdelen\JwtAuth\Controllers\UserController@ban']);
+    $api->get('users/{hash}/unban', ['as' => 'users.unban', 'uses' => 'Onderdelen\JwtAuth\Controllers\UserController@unban']);
+
+    /*
+     * Groups
+     *
+     */
     $api->get('groups', ['as' => 'groups.index', 'uses' => 'Onderdelen\JwtAuth\Controllers\GroupController@index']);
+    // // $api->get('groups/create', ['as' => 'groups.create', 'uses' => 'Onderdelen\JwtAuth\Controllers\GroupController@create']);
     $api->post('groups', ['as' => 'groups.store', 'uses' => 'Onderdelen\JwtAuth\Controllers\GroupController@store']);
-    $api->get('groups/{hash}',
-        ['as' => 'groups.show', 'uses' => 'Onderdelen\JwtAuth\Controllers\GroupController@show']);
-    $api->delete('groups/{hash}',
-        ['as' => 'groups.destroy', 'uses' => 'Onderdelen\JwtAuth\Controllers\GroupController@destroy']);
+    $api->get('groups/{hash}', ['as' => 'groups.show', 'uses' => 'Onderdelen\JwtAuth\Controllers\GroupController@show']);
+    $api->get('groups/{hash}/edit', ['as' => 'groups.edit', 'uses' => 'Onderdelen\JwtAuth\Controllers\GroupController@edit']);
+    $api->put('groups/{hash}', ['as' => 'groups.update', 'uses' => 'Onderdelen\JwtAuth\Controllers\GroupController@update']);
+    $api->delete('groups/{hash}', ['as' => 'groups.destroy', 'uses' => 'Onderdelen\JwtAuth\Controllers\GroupController@destroy']);
 });
 
 // Protected with JWT
 $api->version('v1', ['middleware' => 'api.auth'], function ($api) {
+    /*
+     * Authentication
+     *
+     */
     $api->get('authenticate', 'Onderdelen\JwtAuth\Controllers\AuthenticateController@index');
     $api->get('authenticate/user', 'Onderdelen\JwtAuth\Controllers\AuthenticateController@getAuthenticatedUser');
 });
 
-/*
-Route::group(['namespace' => 'Cerberus\Controllers'], function () {
-
-    // Cerberus Session Routes
-    Route::get('login', ['as' => 'cerberus.login', 'uses' => 'SessionController@create']);
-    Route::get('logout', ['as' => 'cerberus.logout', 'uses' => 'SessionController@destroy']);
-    Route::get('sessions/create', ['as' => 'cerberus.session.create', 'uses' => 'SessionController@create']);
-    Route::post('sessions/store', ['as' => 'cerberus.session.store', 'uses' => 'SessionController@store']);
-    Route::delete('sessions/destroy', ['as' => 'cerberus.session.destroy', 'uses' => 'SessionController@destroy']);
-
-    // Cerberus Registration
-    Route::get('register', ['as' => 'cerberus.register.form', 'uses' => 'RegistrationController@registration']);
-    Route::post('register', ['as' => 'cerberus.register.user', 'uses' => 'RegistrationController@register']);
-    Route::get('users/activate/{hash}/{code}', ['as' => 'cerberus.activate', 'uses' => 'RegistrationController@activate']);
-    Route::get('reactivate', ['as' => 'cerberus.reactivate.form', 'uses' => 'RegistrationController@resendActivationForm']);
-    Route::post('reactivate', ['as' => 'cerberus.reactivate.send', 'uses' => 'RegistrationController@resendActivation']);
-    Route::get('forgot', ['as' => 'cerberus.forgot.form', 'uses' => 'RegistrationController@forgotPasswordForm']);
-    Route::post('forgot', ['as' => 'cerberus.reset.request', 'uses' => 'RegistrationController@sendResetPasswordEmail']);
-    Route::get('reset/{hash}/{code}', ['as' => 'cerberus.reset.form', 'uses' => 'RegistrationController@passwordResetForm']);
-    Route::post('reset/{hash}/{code}', ['as' => 'cerberus.reset.password', 'uses' => 'RegistrationController@resetPassword']);
-
-    // Cerberus Profile
-    Route::get('profile', ['as' => 'cerberus.profile.show', 'uses' => 'ProfileController@show']);
-    Route::get('profile/edit', ['as' => 'cerberus.profile.edit', 'uses' => 'ProfileController@edit']);
-    Route::put('profile', ['as' => 'cerberus.profile.update', 'uses' => 'ProfileController@update']);
-    Route::post('profile/password', ['as' => 'cerberus.profile.password', 'uses' => 'ProfileController@changePassword']);
-
-    // Cerberus Users
-    Route::get('users', ['as' => 'cerberus.users.index', 'uses' => 'UserController@index']);
-    Route::get('users/create', ['as' => 'cerberus.users.create', 'uses' => 'UserController@create']);
-    Route::post('users', ['as' => 'cerberus.users.store', 'uses' => 'UserController@store']);
-    Route::get('users/{hash}', ['as' => 'cerberus.users.show', 'uses' => 'UserController@show']);
-    Route::get('users/{hash}/edit', ['as' => 'cerberus.users.edit', 'uses' => 'UserController@edit']);
-    Route::post('users/{hash}/password', ['as' => 'cerberus.password.change', 'uses' => 'UserController@changePassword']);
-    Route::post('users/{hash}/memberships', ['as' => 'cerberus.users.memberships', 'uses' => 'UserController@updateGroupMemberships']);
-    Route::put('users/{hash}', ['as' => 'cerberus.users.update', 'uses' => 'UserController@update']);
-    Route::delete('users/{hash}', ['as' => 'cerberus.users.destroy', 'uses' => 'UserController@destroy']);
-    Route::get('users/{hash}/suspend', ['as' => 'cerberus.users.suspend', 'uses' => 'UserController@suspend']);
-    Route::get('users/{hash}/unsuspend', ['as' => 'cerberus.users.unsuspend', 'uses' => 'UserController@unsuspend']);
-    Route::get('users/{hash}/ban', ['as' => 'cerberus.users.ban', 'uses' => 'UserController@ban']);
-    Route::get('users/{hash}/unban', ['as' => 'cerberus.users.unban', 'uses' => 'UserController@unban']);
-
-    // Cerberus Groups
-    Route::get('groups', ['as' => 'cerberus.groups.index', 'uses' => 'GroupController@index']);
-    Route::get('groups/create', ['as' => 'cerberus.groups.create', 'uses' => 'GroupController@create']);
-    Route::post('groups', ['as' => 'cerberus.groups.store', 'uses' => 'GroupController@store']);
-    Route::get('groups/{hash}', ['as' => 'cerberus.groups.show', 'uses' => 'GroupController@show']);
-    Route::get('groups/{hash}/edit', ['as' => 'cerberus.groups.edit', 'uses' => 'GroupController@edit']);
-    Route::put('groups/{hash}', ['as' => 'cerberus.groups.update', 'uses' => 'GroupController@update']);
-    Route::delete('groups/{hash}', ['as' => 'cerberus.groups.destroy', 'uses' => 'GroupController@destroy']);
-
-});
-*/

28e33a49a680414b368c4bb722d95585226daeae
anonymoussc <50c5ac69@opayq.com>
2016-01-22 14:33:14 +0700

    New user-show-detail @ directive

---
+++ angular/directives/user-show-detail/user-show-detail.directive.js
@@ -0,0 +1,62 @@
+/**
+ * user-show-detail.scss
+ * Created by anonymous on 22/01/16 14:14.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('jwtAuth')
+        .directive('userShowDetail', userShowDetail);
+
+    userShowDetail.$inject = [];
+
+    /* @ngInject */
+    function userShowDetail() {
+        var directive = {
+            bindToController: true,
+            controller      : UserShowDetailController,
+            controllerAs    : 'ctrl',
+            link            : link,
+            restrict        : 'EA',
+            scope           : {
+                id: '=id'
+            },
+            templateUrl     : function(elem, attr) {
+                return attr.template;
+            }
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+
+        }
+    }
+
+    UserShowDetailController.$inject = ['Users'];
+
+    /* @ngInject */
+    function UserShowDetailController(Users) {
+        var vm = this;
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            var id;
+            if (_.isString(vm.id)) {
+                id = parseInt(vm.id);
+            } else {
+                id = vm.id;
+            }
+
+            Users.get(id).then(function(response) {
+                vm.records = response.data;
+            });
+        }
+    }
+
+})();
+

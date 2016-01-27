947f6735f42d2109bbf2dcb71df36c04b7678eba
anonymoussc <50c5ac69@opayq.com>
2016-01-27 10:26:05 +0700

    Modified seneschal.module@ module

--- angular/seneschal.module.js
+++ angular/seneschal.module.js
@@ -1,9 +1,13 @@
 /**
+ * seneschal.module.js
  * Created by anonymous on 08/12/15 17:00.
  */
 
 (function() {
     'use strict';
+
+    angular
+        .module('seneschal', ['jwtAuth']);
 
     angular
         .module('jwtAuth', [

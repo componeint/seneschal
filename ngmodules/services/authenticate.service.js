626836c873410bba16cb2a195f1d5fd57cc09ea2
anonymoussc <50c5ac69@opayq.com>
2016-02-06 06:01:22 +0700

    Modified authenticate.service

--- ngmodules/services/authenticate.service.js
+++ ngmodules/services/authenticate.service.js
@@ -2,3 +2,49 @@
  * authenticate.service
  * Created by anonymous on 31/01/16 22:45.
  */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('jwtAuth')
+        .factory('authenticate', authenticate);
+
+    authenticate.$inject = [];
+
+    /* @ngInject */
+    function authenticate() {
+        var service = {
+            authenticate        : authenticate,
+            getAuthenticatedUser: getAuthenticatedUser,
+            create              : create,
+            store               : store,
+            destroy             : destroy
+        };
+        return service;
+
+        ////////////////
+
+        function authenticate() {
+            //
+        }
+
+        function getAuthenticatedUser() {
+            //
+        }
+
+        function create() {
+            //
+        }
+
+        function store() {
+            //
+        }
+
+        function destroy() {
+            //
+        }
+    }
+
+})();
+

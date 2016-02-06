cd310279725f86a8caa0c210b3038e82e156a3a9
anonymoussc <50c5ac69@opayq.com>
2016-02-06 19:15:13 +0700

    Modified profile.service

--- ngmodules/services/profile.service.js
+++ ngmodules/services/profile.service.js
@@ -2,3 +2,44 @@
  * profile.service
  * Created by anonymous on 01/02/16 7:21.
  */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('jwtAuth')
+        .factory('Profile', Profile);
+
+    Profile.$inject = [];
+
+    /* @ngInject */
+    function Profile() {
+        var service = {
+            show          : show,
+            edit          : edit,
+            update        : update,
+            changePassword: changePassword
+        };
+        return service;
+
+        ////////////////
+
+        function show() {
+            //
+        }
+
+        function edit() {
+            //
+        }
+
+        function update() {
+            //
+        }
+
+        function changePassword() {
+            //
+        }
+    }
+
+})();
+

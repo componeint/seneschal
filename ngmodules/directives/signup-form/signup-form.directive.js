cc88fbf709bc932276c96ead575ee6b2de3d2e4f
anonymoussc <50c5ac69@opayq.com>
2016-02-20 20:43:11 +0700

    Updated sign-up

--- ngmodules/directives/signup-form/signup-form.directive.js
+++ ngmodules/directives/signup-form/signup-form.directive.js
@@ -14,43 +14,60 @@
 
     /* @ngInject */
     function signupForm() {
-        var directive = {
-            bindToController: true,
-            controller      : SignupFormController,
-            controllerAs    : 'ctrl',
-            link            : link,
-            restrict        : 'EA',
-            scope           : {
-                successredirect: '@'
-            },
-            templateUrl     : function(elem, attr) {
-                return attr.template;
-            }
-        };
+
+        var
+            directive = {
+                bindToController: true,
+                controller      : SignupFormController,
+                controllerAs    : '$ctrl',
+                link            : link,
+                restrict        : 'EA',
+                scope           : {
+                    successStateRedirect: '@'
+                },
+                templateUrl     : function(elem, attr) {
+                    return attr.template;
+                }
+            };
+
         return directive;
 
         function link(scope, element, attrs) {
-
+            //
         }
+
     }
 
-    SignupFormController.$inject = ['$location', '$state', '$auth', 'ToastService'];
+    SignupFormController.$inject = ['$state', '$auth', 'Toast', 'logService'];
 
     /* @ngInject */
-    function SignupFormController($location, $state, $auth, ToastService) {
-        var vm = this;
+    function SignupFormController($state, $auth, Toast, logService) {
+
+        var vm            = this;
+        var stateRedirect = _.isEmpty(vm.successStateRedirect) ? 'app.home' : vm.successStateRedirect;
+
         vm.signup = signup;
+
 
         ////////////////
 
         function signup() {
+
             $auth.signup(vm.user).then(function(response) {
+
                 $auth.setToken(response);
-                $state.go(vm.successredirect);
-                ToastService.show('You have successfully created a new account and have been signed-in');
+
+                $state.go(stateRedirect);
+
+                Toast.show('You have successfully created a new account and have been signed-in');
+
             }).catch(function(response) {
-                ToastService.error(response.data.message);
+
+                logService.error(error);
+                logService.debug(error);
+
             });
+
         }
 
     }

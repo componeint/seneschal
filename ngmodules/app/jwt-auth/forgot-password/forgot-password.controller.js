86277de1fceaf112a780aaa592c15a2ab9e3fd9f
anonymoussc <50c5ac69@opayq.com>
2016-02-20 22:39:19 +0700

    Add forgot-password@ features

---
+++ ngmodules/app/jwt-auth/forgot-password/forgot-password.controller.js
@@ -0,0 +1,32 @@
+/**
+ * forgot-password.controller.js
+ * Created by anonymous on 20/02/16 21:57.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('jwtAuth')
+        .controller('JwtAuthForgotPasswordController', JwtAuthForgotPasswordController);
+
+    JwtAuthForgotPasswordController.$inject = [];
+
+    /* @ngInject */
+    function JwtAuthForgotPasswordController() {
+
+        var vm   = this;
+
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            //
+        }
+
+    }
+
+})();
+

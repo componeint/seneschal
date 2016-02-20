703486bafde528576ba5ff3b93e77b0efc1cd372
anonymoussc <50c5ac69@opayq.com>
2016-02-21 06:30:35 +0700

    Add lang

---
+++ src/lang/en/pages.php
@@ -0,0 +1,65 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Language strings for views
+    |--------------------------------------------------------------------------
+    */
+
+    'flashsuccess'    => 'Success',
+
+    'flasherror'    => 'Error',
+
+    'flashwarning'    => 'Warning',
+
+    'flashinfo'        => 'FYI',
+
+    'register'        => 'Register',
+
+    'login'            => "Login",
+
+    'logout'        => "Logout",
+
+    'home'            => "Home",
+
+    'users'        => 'User|Users',
+
+    'groups'        => "Groups",
+
+    'helloworld'    => "Hello World!",
+
+    'description'    => "This is an example of <a href=\"https://github.com/laravel/laravel\">Laravel 5.1</a> running with <a href=\"https://github.com/einherjars/carbuncle\">Carbuncle 2.0</a> and <a href=\"http://getbootstrap.com/\">Bootstrap 3.0</a>.",
+
+    'loginstatus'    => "You are currently logged in.",
+
+    'sessiondata'    => "Session Data",
+
+    'currentusers'    => "Current Users",
+
+    'options'        => 'Options',
+
+    'status'        => "Status",
+
+    'active'        => "Active",
+
+    'notactive'        => "Not Active",
+
+    'suspended'        => "Suspended",
+
+    'banned'        => "Banned",
+
+    'actionedit'    => "Edit",
+
+    'actionsuspend'    => "Suspend",
+
+    'actionunsuspend' => "Un-Suspend",
+
+    'actionban'        => "Ban",
+
+    'actionunban'    => "Un-Ban",
+
+    'actiondelete'    => "Delete",
+
+);
---
+++ src/lang/nl/pages.php
@@ -0,0 +1,65 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Language strings for views
+    |--------------------------------------------------------------------------
+    */
+
+    'flashsuccess'    => 'Succes',
+
+    'flasherror'    => 'Fout',
+
+    'flashwarning'    => 'Waarschuwing',
+
+    'flashinfo'        => 'FYI',
+
+    'register'        => 'Registreer',
+
+    'login'            => "Aanmelden",
+
+    'logout'        => "Uitloggen",
+
+    'home'            => "Home",
+
+    'users'        => 'User|Users',
+
+    'groups'        => "Groepen",
+
+    'helloworld'    => "Hello world!",
+
+    'description'    => "Dit is een voorbeeld van <a href=\"https://github.com/laravel/laravel\"> Laravel 5.1</a> met <a href=\"https://github.com/einherjars/carbuncle\"> Carbuncle 2.0</a> en <a href=\"http://getbootstrap.com/\"> Bootstrap 3.0</a>.",
+
+    'loginstatus'    => "U bent momenteel ingelogd.",
+
+    'sessiondata'    => "Sessiegegevens",
+
+    'currentusers'    => "Huidige gebruikers",
+
+    'options'        => 'Optie\'s',
+
+    'status'        => "Status",
+
+    'active'        => "Actief",
+
+    'notactive'        => "Niet actief",
+
+    'suspended'        => "Geblokkeerd",
+
+    'banned'        => "Verbannen",
+
+    'actionedit'    => "Bewerken",
+
+    'actionsuspend'    => "Blokkeer",
+
+    'actionunsuspend' => "Deblokkeer",
+
+    'actionban'        => "Verban",
+
+    'actionunban'    => "Einde ban",
+
+    'actiondelete'    => "Wissen",
+
+);

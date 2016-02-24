5c7bb26a69fb04e0eb3da1e893c7b9b1cd996855
anonymoussc <50c5ac69@opayq.com>
2016-02-25 00:40:55 +0700

    Updated password-*@ directives

---
+++ resources/angular-material/directives/password-match/password-match.directive.js
@@ -0,0 +1,37 @@
+/**
+ * password-match.directive.js
+ * Created by anonymous on 01/01/16 18:51.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('seneschal')
+        .directive('passwordMatch', passwordMatch);
+
+    passwordMatch.$inject = [];
+
+    /* @ngInject */
+    function passwordMatch() {
+        var directive = {
+            require: 'ngModel',
+            link   : link,
+            scope  : {
+                otherModelValue: '=passwordMatch'
+            }
+        };
+        return directive;
+
+        function link(scope, element, attributes, ngModel) {
+            ngModel.$validators.compareTo = function(modelValue) {
+                return modelValue === scope.otherModelValue;
+            };
+            scope.$watch('otherModelValue', function() {
+                ngModel.$validate();
+            });
+        }
+    }
+
+})();
+

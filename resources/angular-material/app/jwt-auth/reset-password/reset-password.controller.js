8c11a131d7ef0f76ad9680a2b3a54c9e31ab9a57
anonymoussc <50c5ac69@opayq.com>
2016-02-24 17:58:52 +0700

    Add reset-password

---
+++ resources/angular-material/app/jwt-auth/reset-password/reset-password.controller.js
@@ -0,0 +1,36 @@
+/**
+ * reset-password.controller
+ * Created by anonymous on 24/02/16 3:43.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('seneschal')
+        .controller('JwtAuthResetPasswordController', JwtAuthResetPasswordController);
+
+    JwtAuthResetPasswordController.$inject = ['$stateParams'];
+
+    /* @ngInject */
+    function JwtAuthResetPasswordController($stateParams) {
+
+        var vm = this;
+
+        vm.data = {
+            hash: $stateParams.hash,
+            code: $stateParams.code
+        };
+
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            //
+        }
+    }
+
+})();
+

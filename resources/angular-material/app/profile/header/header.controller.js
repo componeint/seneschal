d7e4a15fc0c6ac526baea2937843b7ccb924d787
anonymoussc <50c5ac69@opayq.com>
2016-02-27 01:11:19 +0700

    Add header.controller

---
+++ resources/angular-material/app/profile/header/header.controller.js
@@ -0,0 +1,94 @@
+/**
+ * header.controller.js
+ * Created by anonymous on 27/02/16 1:03.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('seneschal')
+        .controller('ProfileHeaderController', ProfileHeaderController);
+
+    ProfileHeaderController.$inject = ['$timeout', '$mdSidenav', '$log'];
+
+    /* @ngInject */
+    function ProfileHeaderController($timeout, $mdSidenav, $log) {
+
+        var vm = this;
+
+        vm.toggleLeft  = buildDelayedToggler('left');
+        vm.toggleRight = buildToggler('right');
+        vm.isOpenRight = function() {
+            return $mdSidenav('right').isOpen();
+        };
+
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            //
+        }
+
+        /**
+         * Build handler to open/close a SideNav; when animation finishes
+         * report completion in console
+         */
+        function buildDelayedToggler(navID) {
+
+            return debounce(function() {
+
+                $mdSidenav(navID).toggle().then(function() {
+                    // $log.debug("toggle " + navID + " is done");
+                });
+
+            }, 200);
+
+        }
+
+        function buildToggler(navID) {
+
+            return function() {
+
+                $mdSidenav(navID).toggle().then(function() {
+                    // $log.debug("toggle " + navID + " is done");
+                });
+
+            };
+
+        }
+
+        /**
+         * Supplies a function that will continue to operate until the
+         * time is up.
+         */
+        function debounce(func, wait, context) {
+
+            var timer;
+
+            return function debounced() {
+
+                var
+                    context = vm,
+                    args    = Array.prototype.slice.call(arguments);
+
+                $timeout.cancel(timer);
+
+                timer = $timeout(function() {
+
+                    timer = undefined;
+
+                    func.apply(context, args);
+
+                }, wait || 10);
+
+            };
+
+        }
+
+    }
+
+})();
+

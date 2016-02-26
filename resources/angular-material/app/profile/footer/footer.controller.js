87f4dea729ee2879edd373ac101a2db594133eee
anonymoussc <50c5ac69@opayq.com>
2016-02-27 01:52:11 +0700

    Add footer

---
+++ resources/angular-material/app/profile/footer/footer.controller.js
@@ -0,0 +1,31 @@
+/**
+ * footer.controller.js
+ * Created by anonymous on 27/02/16 1:50.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('seneschal')
+        .controller('ProfileFooterController', ProfileFooterController);
+
+    ProfileFooterController.$inject = [];
+
+    /* @ngInject */
+    function ProfileFooterController() {
+
+        var vm   = this;
+
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            //
+        }
+    }
+
+})();
+

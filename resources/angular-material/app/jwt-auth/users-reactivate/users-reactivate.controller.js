00ffc3d1bbd5257c6fffa0fc349f2c168956f34d
anonymoussc <50c5ac69@opayq.com>
2016-03-04 01:34:11 +0700

    Add users-reactivate.controller

---
+++ resources/angular-material/app/jwt-auth/users-reactivate/users-reactivate.controller.js
@@ -0,0 +1,32 @@
+/**
+ * users-reactivate.controller.js
+ * Created by anonymous on 03/03/16 19:04.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('seneschal')
+        .controller('JwtAuthUsersReactivateController', JwtAuthUsersReactivateController);
+
+    JwtAuthUsersReactivateController.$inject = [];
+
+    /* @ngInject */
+    function JwtAuthUsersReactivateController() {
+
+        // noinspection JSUnusedAssignment
+        // var vm = this;
+
+        /* @Init */
+        activate();
+
+        ////////////////
+
+        function activate() {
+            //
+        }
+    }
+
+})();
+

626ac1f57d4bc6cfe37654f20ca2385194e3c8df
anonymoussc <50c5ac69@opayq.com>
2016-03-04 01:35:42 +0700

    Add users-reactivate-form

---
+++ resources/angular-material/directives/users-reactivate-form/users-reactivate-form.directive.js
@@ -0,0 +1,87 @@
+/**
+ * users-reactivate-form.directive
+ * Created by anonymous on 03/03/16 23:39.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('seneschal')
+        .directive('usersReactivateForm', usersReactivateForm);
+
+    usersReactivateForm.$inject = [];
+
+    /* @ngInject */
+    function usersReactivateForm() {
+
+        var
+            directive = {
+                bindToController: true,
+                controller      : UsersReactivateFormController,
+                controllerAs    : '$ctrl',
+                link            : link,
+                restrict        : 'EA',
+                scope           : {},
+                templateUrl     : function(elem, attr) {
+                    return attr.template;
+                }
+            };
+
+        return directive;
+
+        function link(scope, element, attrs) {
+
+        }
+    }
+
+    UsersReactivateFormController.$inject = ['API', '$state', 'logService', '$timeout', 'Toast'];
+
+    /* @ngInject */
+    function UsersReactivateFormController(API, $state, logService, $timeout, Toast) {
+
+        // noinspection JSUnusedAssignment
+        var vm            = this;
+        var stateRedirect = _.isEmpty(vm.successStateRedirect) ? 'client.home' : vm.successStateRedirect;
+
+        vm.reactivate     = reactivate;
+
+        /* @Init */
+        activate();
+
+        ////////////////
+
+        function activate() {
+            //
+        }
+
+        function reactivate() {
+
+            vm.formData = {
+                email: vm.email
+            };
+
+            API.one('reactivate').doPOST(vm.formData).then(function(response) {
+
+                if (!response.status.error) {
+                    Toast.show('Email sent');
+
+                    /*
+                     $timeout(function() {
+                     $state.go(stateRedirect);
+                     }, 7000);
+                     */
+                }
+            }, function(error) {
+
+                logService.error(error);
+                logService.debug(error);
+
+            });
+
+        }
+
+    }
+
+})();
+

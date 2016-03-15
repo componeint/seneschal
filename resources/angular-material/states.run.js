e50ee5f87d163be836704d2eaca887fd3910ddc4
anonymoussc <50c5ac69@opayq.com>
2016-03-15 08:26:15 +0700

    Modified states.run

--- resources/angular-material/states.run.js
+++ resources/angular-material/states.run.js
@@ -8,19 +8,17 @@
 
     angular.module('seneschal').run(function($rootScope, $state) {
         $rootScope.$on('$stateChangeStart', function(event, toState) {
+
             var user = JSON.parse(localStorage.getItem('user'));
+
             if (user) {
                 $rootScope.authenticated = true;
                 $rootScope.currentUser   = user;
-
-                /*
-                if ((toState.name === 'jwtauth.signin') || (toState.name === 'jwtauth.register')) {
-                    event.preventDefault();
-                    $state.go('client.home');
-                }
-                */
             }
+
         });
+
     });
+
 })();
 

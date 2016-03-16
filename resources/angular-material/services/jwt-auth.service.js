da8ba37577bacb2a8bb437b94eeb97123e987bf3
anonymoussc <50c5ac69@opayq.com>
2016-03-16 22:50:41 +0700

    Modified jwt-auth.service

--- resources/angular-material/services/jwt-auth.service.js
+++ resources/angular-material/services/jwt-auth.service.js
@@ -14,38 +14,34 @@
 
     /* @ngInject */
     function jwtAuthService($rootScope, $http, $auth) {
+
         var
             service = {
                 getUsers: getUsers,
                 signout : signout
             };
+
         return service;
 
         ////////////////
 
         function getUsers() {
             return $http.get('api/authenticate');
-            /*
-             .then(getUsersComplete)
-             .catch(getUsersFailed);
-
-             function getUsersComplete(response) {
-             return response.data.results;
-             }
-
-             function getUsersFailed(error) {
-             console.log('XHR failed for getUser.' + error.data);
-             }
-             */
         }
 
         function signout() {
+
             return $auth.logout().then(function() {
+
                 localStorage.removeItem('user');
+
                 $rootScope.authenticated = false;
                 $rootScope.currentUser   = null;
+
             });
+
         }
+
     }
 
 })();

b75d2cacee2bc4ab438ddb6d804141e7c98fa8e0
anonymoussc <50c5ac69@opayq.com>
2016-03-17 22:13:37 +0700

    Modified validators

--- src/validators.php
+++ src/validators.php
@@ -5,7 +5,6 @@
  * by anonymous on 12/01/16 22:28.
  */
 
-// As seen on http://blog.elenakolevska.com/laravel-alpha-validator-that-allows-spaces/
 Validator::extend('alpha_spaces', function ($attribute, $value) {
     return preg_match('/^[\pL\s]+$/u', $value);
 });

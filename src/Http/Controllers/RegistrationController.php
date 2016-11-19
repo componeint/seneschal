ba14dd66bbdede1f367ebd7c0cf6c41c6df215d1
anonymoussc <50c5ac69@opayq.com>
2016-11-20 00:59:28 +0700

    Modified ./src/Http/Controllers/RegistrationController.php

--- src/Http/Controllers/RegistrationController.php
+++ src/Http/Controllers/RegistrationController.php
@@ -25,25 +25,13 @@ use Redirect;
 use Session;
 use Config;
 
-/**
- * Class RegistrationController
- * @package Componeint\Seneschal\Controllers
- */
 class RegistrationController extends Controller
 {
     use SeneschalRedirectionTrait;
     use SeneschalViewfinderTrait;
 
-    /**
-     * @param UserRepositoryInterface  $userRepository
-     * @param GroupRepositoryInterface $groupRepository
-     * @param HashidsManager           $hashids
-     */
-    public function __construct(
-        UserRepositoryInterface $userRepository,
-        GroupRepositoryInterface $groupRepository,
-        HashidsManager $hashids
-    ) {
+    public function __construct(UserRepositoryInterface $userRepository, GroupRepositoryInterface $groupRepository, HashidsManager $hashids)
+    {
         $this->userRepository  = $userRepository;
         $this->groupRepository = $groupRepository;
         $this->hashids         = $hashids;
@@ -54,33 +42,19 @@ class RegistrationController extends Controller
         // $this->middleware('jwt.auth', ['except' => ['register']]);
     }
 
-    /**
-     * Show the registration form, if registration is allowed
-     *
-     * @return \Response
-     */
     public function registration()
     {
-        // Is this user already signed in? If so redirect to the post login route
         if (Sentry::check()) {
             return $this->redirectTo('session_store');
         }
 
-        //If registration is currently disabled, show a message and redirect home.
         if (!config('seneschal.registration', false)) {
             return $this->redirectTo(['route' => 'home'], ['error' => trans('Seneschal::users.inactive_reg')]);
         }
 
-        // All clear - show the registration form.
         return $this->viewFinder('Seneschal::users.register');
     }
 
-    /**
-     * Process a registration request
-     *
-     * @param RegisterRequest $request
-     * @return \Illuminate\Http\JsonResponse
-     */
     public function register(RegisterRequest $request)
     {
         // Gather input
@@ -113,90 +87,43 @@ class RegistrationController extends Controller
         return response()->json(compact('token'));
     }
 
-    /**
-     * Activate a new user
-     *
-     * @param $hash
-     * @param $code
-     * @return \Response
-     */
     public function activate($hash, $code)
     {
-        // Decode the hashid
-        $id = $this->hashids->decode($hash)[0];
-
-        // Attempt the activation
+        $id     = $this->hashids->decode($hash)[0];
         $result = $this->userRepository->activate($id, $code);
 
-        // It worked!  Use config to determine where we should go.
         return $this->redirectViaResponse('registration_activated', $result);
     }
 
-    /**
-     * Show the 'Resend Activation' form
-     *
-     * @return \Response
-     */
     public function resendActivationForm()
     {
         // return $this->viewFinder('Seneschal::users.resend');
     }
 
-    /**
-     * Process resend activation request
-     *
-     * @param EmailRequest $request
-     * @return \Response
-     */
     public function resendActivation(EmailRequest $request)
     {
-        // Resend the activation email
         $result = $this->userRepository->resend(['email' => e($request->get('email'))]);
 
-        // It worked!  Use config to determine where we should go.
         // return $this->redirectViaResponse('registration_resend', $result);
         return response()->success([$result]);
     }
 
-    /**
-     * Display the "Forgot Password" form
-     *
-     * @return \Response
-     */
     public function forgotPasswordForm()
     {
         return $this->viewFinder('Seneschal::users.forgot');
     }
 
-    /**
-     * Process Forgot Password request
-     *
-     * @param EmailRequest $request
-     * @return \Response
-     */
     public function sendResetPasswordEmail(EmailRequest $request)
     {
-        // Send Password Reset Email
         $result = $this->userRepository->triggerPasswordReset(e($request->get('email')));
 
-        // It worked!  Use config to determine where we should go.
         // return $this->redirectViaResponse('registration_reset_triggered', $result);
         return response()->success([$result]);
     }
 
-    /**
-     * A user is attempting to reset their password
-     *
-     * @param $hash
-     * @param $code
-     * @return \Response
-     */
     public function passwordResetForm($hash, $code)
     {
-        // Decode the hashid
-        $id = $this->hashids->decode($hash)[0];
-
-        // Validate Reset Code
+        $id     = $this->hashids->decode($hash)[0];
         $result = $this->userRepository->validateResetCode($id, $code);
 
         if (!$result->isSuccessful()) {
@@ -214,26 +141,12 @@ class RegistrationController extends Controller
         return response()->success([$result]);
     }
 
-    /**
-     * Process a password reset form submission
-     *
-     * @param ResetPasswordRequest $request
-     * @param                      $hash
-     * @param                      $code
-     * @return \Response
-     */
     public function resetPassword(ResetPasswordRequest $request, $hash, $code)
     {
-        // Decode the hashid
-        $id = $this->hashids->decode($hash)[0];
-
-        // Gather input data
-        $data = Input::only('password', 'password_confirmation');
-
-        // Change the user's password
+        $id     = $this->hashids->decode($hash)[0];
+        $data   = Input::only('password', 'password_confirmation');
         $result = $this->userRepository->resetPassword($id, $code, e($data['password']));
 
-        // It worked!  Use config to determine where we should go.
         // return $this->redirectViaResponse('registration_reset_complete', $result);
         return response()->success([$result]);
     }
